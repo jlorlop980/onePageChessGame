8be4ad7a1320c667c99b44ce11dbf2421713f308
jlorlop980 <jalorenzo@iesrafaelalberti.es>
2023-03-02 03:30:18 +0100

    almost finished

--- src/js/main.js
+++ src/js/main.js
@@ -14,33 +14,51 @@ document.getElementById("cards").onmousemove= e => {
 
 //Todo lo relativo a los fondos
 
-const track= document.getElementById("image-track");
+const track = document.getElementById("image-track");
 
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = (window.innerWidth / 2)-10;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
 
-window.onmousemove = e =>{
-    if(track.dataset.mouseDownAt==="0") return;
+// añadido para que tambien funcione en movil
 
-    const mouseDelta =parseFloat(track.dataset.mouseDownAt)-e.clientX,
-        maxDelta=window.innerWidth/2
+window.onmousedown = e => handleOnDown(e);
 
-    const percentage = (mouseDelta/maxDelta)*-100,
-        nextPercentageU= parseFloat(track.dataset.prevPercentage)+ percentage,
-        nextPercentage=Math.max(Math.min(nextPercentageU,0),-100)
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
 
 
-    track.dataset.percentage = nextPercentage;
-
-    track.style.transform = `translate(${nextPercentage}%,-50%)`;
-
-    for(const image of track.getElementsByClassName("image")){
-        image.style.objectPosition = `${nextPercentage+100}% 50%`
-    }
-}
+// para los personajes
